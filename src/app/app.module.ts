75555c105f79e3586390f5d98628cbd6629a80ad
denisesantosdev <denisesantos427@gmail.com>
2023-10-06 18:33:56 -0300

    feat: components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchScreenComponent } from './search-screen/search-screen.component';
+import { NameSectionComponent } from './name-section/name-section.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { StatComponent } from './stat/stat.component';
+import { RepoCardComponent } from './repo-card/repo-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchScreenComponent,
+    NameSectionComponent,
+    TabsComponent,
+    StatComponent,
+    RepoCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/name-section/name-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-section',
+  templateUrl: './name-section.component.html',
+  styleUrls: ['./name-section.component.css']
+})
+export class NameSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
