4679a9bc524b87b948290ad572b59337a7b68d06
denisesantosdev <denisesantos427@gmail.com>
2023-10-23 19:14:14 -0300

    feat: basic loader implementation

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit {
-
+  APIResponse:boolean = true
   constructor() { }
 
   ngOnInit(): void {
