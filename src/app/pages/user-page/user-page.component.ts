d1d2bbbe85d70c1fe4d49700e6bc01fe6d0c9778
denisesantosdev <denisesantos427@gmail.com>
2023-11-06 19:11:04 -0300

    feat: add api error handling

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserData } from 'src/app/models/userData';
@@ -23,14 +24,26 @@ export class UserPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
     this.service.fetchUser(this.searhQuery.username).subscribe({
       next: (res) => {
         console.log(res);
-       this.user = this.service.shapeUserData(res)
-       console.log(this.user);
+        this.user = this.service.shapeUserData(res);
+        console.log(this.user);
       },
-      error: (err) => console.log(err),
+      error: (err) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 404) {
+            // Handle 404 Not Found error (User not found)
+            console.error('User not found!');
+          } else {
+            // Handle other HTTP errors
+            console.error('An error occurred while fetching user data');
+          }
+        } else {
+          // Handle non-HTTP errors
+          console.error('An unexpected error occurred');
+        }
+      },
     });
   }
 }
