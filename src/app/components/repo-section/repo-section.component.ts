327f3c73e02e7f380c3ca1131cbeee260a651dae
denisesantosdev <denisesantos427@gmail.com>
2023-12-15 20:25:03 -0300

    add search repos functionality

--- src/app/components/repo-section/repo-section.component.ts
+++ src/app/components/repo-section/repo-section.component.ts
@@ -5,22 +5,30 @@ import { GithubRepoService } from 'src/app/services/github-repo.service';
 @Component({
   selector: 'app-repo-section',
   templateUrl: './repo-section.component.html',
-  styleUrls: ['./repo-section.component.css']
+  styleUrls: ['./repo-section.component.css'],
 })
 export class RepoSectionComponent implements OnInit {
-  repoData!: userRepoData[]
+  repoData!: userRepoData[];
+  filteredRepos!: userRepoData[];
+  repoSearchQuery: string = '';
 
-  constructor(private service: GithubRepoService) { }
+  constructor(private service: GithubRepoService) {}
 
   ngOnInit(): void {
     this.service.getRepoData().subscribe({
-      next: () =>{
-        this.repoData = this.service.getRepoShapedData()
+      next: () => {
+        this.repoData = this.service.getRepoShapedData();
+        this.filteredRepos = this.repoData;
       },
       error: (err: any) => {
         console.log(err);
-      }
-    })
+      },
+    });
   }
 
+  searchRepos() {
+    this.filteredRepos = this.repoData.filter((repo) => {
+      return repo.name.includes(this.repoSearchQuery);
+    });
+  }
 }
