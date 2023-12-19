a548cfc7d7085f11583bf58037f08a021ce4f4b6
denisesantosdev <denisesantos427@gmail.com>
2023-12-19 20:56:55 -0300

    add sort by newest and oldest functionality

--- src/app/components/repo-section/repo-section.component.ts
+++ src/app/components/repo-section/repo-section.component.ts
@@ -11,6 +11,7 @@ export class RepoSectionComponent implements OnInit {
   repoData!: userRepoData[];
   filteredRepos!: userRepoData[];
   repoSearchQuery: string = '';
+  selectedSort: string = 'Alphabetically';
 
   constructor(private service: GithubRepoService) {}
 
@@ -19,6 +20,9 @@ export class RepoSectionComponent implements OnInit {
       next: () => {
         this.repoData = this.service.getRepoShapedData();
         this.filteredRepos = this.repoData;
+        this.sortRepos()
+        //console.log(this.filteredRepos);
+        //this.displayLanguageFilters();
       },
       error: (err: any) => {
         console.log(err);
@@ -31,4 +35,54 @@ export class RepoSectionComponent implements OnInit {
       return repo.name.includes(this.repoSearchQuery);
     });
   }
+
+  sortRepos() {
+    console.log(this.selectedSort);
+    //console.log(this.filteredRepos.sort());
+    //if(this.selectedSort==='Alphabetically'){
+    //}
+    if (this.selectedSort === 'Oldest') {
+      this.filteredRepos.sort((a, b) => {
+        const date1: any = new Date(a.created_at);
+        const date2: any = new Date(b.created_at);
+
+        return date1 - date2;
+      });
+    } else if (this.selectedSort === 'Newest') {
+      this.filteredRepos.sort((a, b) => {
+        const date1: any = new Date(a.created_at);
+        const date2: any = new Date(b.created_at);
+
+        return date2 - date1;
+      });
+    } /* else if (this.selectedSort === 'Alphabetically') {
+      this.filteredRepos.sort((a, b) => {
+        const name1: any = new Date(a.name);
+        const name2: any = new Date(b.name);
+
+        return name2 - name1;
+      });
+    } */
+  }
+
+  /* generate language button filters 
+    map repoData return repo.language
+  
+
+  displayLanguageFilters() {
+    // find all the unique language elements 
+    // filter => find 
+    let languages = this.repoData
+      .map((repo) => {
+        return repo.language;
+      })
+      .filter((value, index, array) => array.indexOf(value) === index);
+
+    return languages;
+  }
+
+  
+    filter repo.language === (language)
+    return filteredRepos.length
+  */
 }
